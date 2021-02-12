d9a9df4fc34a5fff7c36b5db5048f9424b4bd6d8
Matthias Van Landuyt <matthias.vanlanduyt@gmail.com>
2021-02-12 19:15:07 +0100

    thumbnail extension??

--- main.py
+++ main.py
@@ -22,17 +22,32 @@ class ThumbnailAdaptor:
                 print('Cannot convert', inputfile)
     
     def create_thumbnail (fpath, size):
-        print(size)
+
+        dir = os.path.dirname(fpath)
+
         isDirectory = os.path.isdir(fpath)
-        print (isDirectory)
+        if isDirectory:
+            thumbnail_dir = os.path.join(fpath, 'thumbnails') 
+        else:
+            thumbnail_dir = os.path.join(dir, 'thumbnails') 
+
+        try:  
+            os.mkdir(thumbnail_dir)  
+        except OSError as error:  
+            print(error)   
+
         if isDirectory:
             doAllFiles = input ('The path is a directory, do you want to create thumbnails for all files? Y or y to continue')
             if doAllFiles[0] in ['Y', 'y']:
                     for entry in os.scandir(fpath):
-                        outputfile = os.path.splitext(entry)[0] + '.thumbnail'
+                        
+                        base = entry.name
+                        fname = os.path.splitext(base)[0]
+                        outputfile = os.path.join(thumbnail_dir, fname) + '.thumbnail'
+
                         if entry != outputfile:
                             try: 
-                                with Image.open(entry) as image:
+                                with Image.open(entry.path) as image:
                                     image.thumbnail(size)
                                     image.save(outputfile, 'JPEG')
                                     print (f'{outputfile} created.')
