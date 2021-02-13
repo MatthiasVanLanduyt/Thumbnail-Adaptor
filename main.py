b2a3929de980e68293be320caff0a686f5db40af
Matthias Van Landuyt <matthias.vanlanduyt@gmail.com>
2021-02-13 17:04:25 +0100

    two separate methods for files and  folders

--- main.py
+++ main.py
@@ -20,53 +20,63 @@ class ThumbnailAdaptor:
                     print('File saved')
             except OSError:
                 print('Cannot convert', inputfile)
-    
+
     def create_thumbnail (fpath, size):
-
-        dir = os.path.dirname(fpath)
-
-        isDirectory = os.path.isdir(fpath)
-        if isDirectory:
-            thumbnail_dir = os.path.join(fpath, 'thumbnails') 
-        else:
-            thumbnail_dir = os.path.join(dir, 'thumbnails') 
-
+        fpath = fpath.strip('\"')
         try:  
+            dir = os.path.dirname(fpath)
+            thumbnail_dir = os.path.join(dir, 'thumbnails') 
+            print(thumbnail_dir)
+            
             os.mkdir(thumbnail_dir)  
         except OSError as error:  
-            print(error)   
+            print(error)
 
-        if isDirectory:
-            doAllFiles = input ('The path is a directory, do you want to create thumbnails for all files? Y or y to continue')
-            if doAllFiles[0] in ['Y', 'y']:
-                    for entry in os.scandir(fpath):
+        try:
+            with Image.open(fpath) as image:
+                base = os.path.basename(fpath)
+                fname = os.path.splitext(base)[0]
+                outputfile = os.path.join(thumbnail_dir, fname) + '_thumbnail.jpg'
+                image = image.resize(size)
+                image.save(outputfile)
+                # Why doesnt this work
+                # image.save(outputfile, 'JPEG')
+                print (f'{outputfile} created.')
+        except OSError as error:
+            print("Cannot convert ", fpath)
+            print(error)     
+
+
+    def create_thumbnail_folder (fpath, size):
+        fpath = fpath.strip('\"')
+        try:  
+            thumbnail_dir = os.path.join(fpath, 'thumbnails') 
+            os.mkdir(thumbnail_dir)  
+        except OSError as error:  
+            print(error)
+
+        for entry in os.scandir(fpath):
                         
-                        base = entry.name
-                        fname = os.path.splitext(base)[0]
-                        outputfile = os.path.join(thumbnail_dir, fname) + '.thumbnail'
+            base = entry.name
+            fname = os.path.splitext(base)[0]
+            outputfile = os.path.join(thumbnail_dir, fname) + '_thumbnail.jpg'
 
-                        if entry != outputfile:
-                            try: 
-                                with Image.open(entry.path) as image:
-                                    image.thumbnail(size)
-                                    image.save(outputfile, 'JPEG')
-                                    print (f'{outputfile} created.')
-                            except OSError:
-                                print("cannot convert", entry)
-        else:
-            inputfile = fpath
-            outputfile = os.path.splitext(inputfile)[0] + '.thumbnail'
-            try: 
-                with Image.open(inputfile) as image:
-                    image.thumbnail(size)
-                    image.save(outputfile, 'JPEG')
-                    print (f'{outputfile} created.')
-            except OSError:
-                print("cannot convert", inputfile)                      
+            if entry != outputfile:
+                try: 
+                    with Image.open(entry.path) as image:
+                        image = image.resize(size)
+                        image.save(outputfile)
+                        # Why doesnt this work
+                        # image.save(outputfile, format = 'jpeg')
+
+                        print (f'{outputfile} created.')
+                except OSError as error:
+                    print("cannot convert ", entry)
+                    print(error)                            
 
 
 if __name__ == '__main__':
     fpath = input('PLease give the path to the directory or file you want to save as jpg: \n')
     ta = ThumbnailAdaptor
     size = (128, 128)
-    ta.create_thumbnail(fpath, size)
+    ta.create_thumbnail_folder(fpath, size)
